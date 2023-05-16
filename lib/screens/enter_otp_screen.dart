c3e6a9687187fb8b8514a550400800a0a17c1049
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-05-16 09:37:42 +0530

    Initial commit

---
+++ lib/screens/enter_otp_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class EnterOtpScreen extends StatelessWidget {
+  const EnterOtpScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
