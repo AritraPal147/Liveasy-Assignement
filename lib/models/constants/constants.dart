67e05b0b39e36fec6b03efcde6ea3839afc12df1
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-05-16 10:26:11 +0530

    Added a small part to 1st screen

---
+++ lib/models/constants/constants.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+class Constants {
+  static const greyTextColor = Color(0xff6a6c7b);
+}
--- lib/screens/select_language_screen.dart
+++ lib/screens/select_language_screen.dart
@@ -1,4 +1,7 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:liveasy_assignment/models/constants/constants.dart';
+import 'package:liveasy_assignment/models/constants/size_config.dart';
 
 class LanguageSelectScreen extends StatefulWidget {
   const LanguageSelectScreen({Key? key}) : super(key: key);
@@ -10,6 +13,52 @@ class LanguageSelectScreen extends StatefulWidget {
 class _LanguageSelectScreenState extends State<LanguageSelectScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    SizeConfig.init(context);
+    return SafeArea(
+      child: Scaffold(
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              width: double.infinity,
+            ),
+            SvgPicture.asset('assets/images/image.svg'),
+            SizedBox(
+              height: SizeConfig.blockSizeVertical * 2,
+            ),
+            Text(
+              'Please Select your Language',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: SizeConfig.blockSizeVertical * 2.5,
+              ),
+            ),
+            SizedBox(
+              height: SizeConfig.blockSizeVertical * 2,
+            ),
+            Text(
+              'You can change the language',
+              style: TextStyle(
+                fontSize: SizeConfig.blockSizeVertical * 1.8,
+                color: Constants.greyTextColor,
+              ),
+            ),
+            Text(
+              'at any time',
+              style: TextStyle(
+                fontSize: SizeConfig.blockSizeVertical * 1.8,
+                color: Constants.greyTextColor,
+              ),
+            ),
+            SizedBox(
+              height: SizeConfig.blockSizeVertical * 2,
+            ),
+
+
+          ],
+        ),
+      ),
+    );
   }
 }
