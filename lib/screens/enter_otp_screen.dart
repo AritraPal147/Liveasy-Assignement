87a40cb77655806d4545396a95172a2d876ca3ce
Aritra Pal <94114400+AritraPal147@users.noreply.github.com>
2023-05-17 17:56:09 +0530

    Modified UI of 3rd screen

--- lib/screens/enter_otp_screen.dart
+++ lib/screens/enter_otp_screen.dart
@@ -1,10 +1,170 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:liveasy_assignment/models/constants/constants.dart';
+import 'package:liveasy_assignment/models/constants/size_config.dart';
+import 'package:liveasy_assignment/models/widgets/rectangle_button.dart';
+import 'package:pinput/pinput.dart';
 
-class EnterOtpScreen extends StatelessWidget {
+class EnterOtpScreen extends StatefulWidget {
   const EnterOtpScreen({Key? key}) : super(key: key);
 
   @override
+  State<EnterOtpScreen> createState() => _EnterOtpScreenState();
+}
+
+class _EnterOtpScreenState extends State<EnterOtpScreen> {
+  // TODO: Bring the phone number from the previous page
+  static const String phoneNumber = '7063393028';
+
+  final pinController = TextEditingController();
+  final focusNode = FocusNode();
+  final formKey = GlobalKey<FormState>();
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    const focusedBorderColor = Color(0xff93d2f3);
+    const fillColor = Color(0xff93d2f3);
+    const borderColor = Color(0xff93d2f3);
+
+    final defaultPinTheme = PinTheme(
+      width: 56,
+      height: 56,
+      textStyle: const TextStyle(
+        fontSize: 22,
+        color: Color.fromRGBO(30, 60, 87, 1),
+      ),
+      decoration: BoxDecoration(
+        border: Border.all(color: borderColor),
+        color: fillColor,
+      ),
+    );
+
+    return SafeArea(
+      child: Scaffold(
+        body: Stack(
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: IconButton(
+                    icon: Icon(
+                      Icons.arrow_back,
+                      color: Constants.greyBorder,
+                      size: SizeConfig.blockSizeVertical * 3,
+                    ),
+                    onPressed: () => Navigator.pop(context),
+                  ),
+                ),
+                SizedBox(
+                  height: SizeConfig.screenHeight * 0.1,
+                  width: double.infinity,
+                ),
+                Text(
+                  'Verify Phone',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: SizeConfig.blockSizeVertical * 2.5,
+                  ),
+                ),
+                SizedBox(
+                  height: SizeConfig.blockSizeVertical * 2,
+                ),
+                Text(
+                  'Code is sent to $phoneNumber',
+                  style: TextStyle(
+                    fontSize: SizeConfig.blockSizeVertical * 1.8,
+                    color: Constants.greyTextColor,
+                  ),
+                ),
+                SizedBox(
+                  height: SizeConfig.blockSizeVertical * 4,
+                ),
+                Form(
+                  key: formKey,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Directionality(
+                        // Specify direction if desired
+                        textDirection: TextDirection.ltr,
+                        child: Pinput(
+                          length: 6,
+                          controller: pinController,
+                          focusNode: focusNode,
+                          androidSmsAutofillMethod:
+                              AndroidSmsAutofillMethod.smsUserConsentApi,
+                          listenForMultipleSmsOnAndroid: true,
+                          defaultPinTheme: defaultPinTheme,
+                          validator: (value) {
+                            return value == '222222' ? null : 'Pin is incorrect';
+                          },
+                          // onClipboardFound: (value) {
+                          //   debugPrint('onClipboardFound: $value');
+                          //   pinController.setText(value);
+                          // },
+                          hapticFeedbackType: HapticFeedbackType.lightImpact,
+                          onCompleted: (pin) {
+                            debugPrint('onCompleted: $pin');
+                          },
+                          onChanged: (value) {
+                            debugPrint('onChanged: $value');
+                          },
+                          cursor: Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            children: [
+                              Container(
+                                margin: const EdgeInsets.only(bottom: 9),
+                                width: 22,
+                                height: 1,
+                                color: focusedBorderColor,
+                              ),
+                            ],
+                          ),
+                          focusedPinTheme: defaultPinTheme.copyWith(
+                            decoration: defaultPinTheme.decoration!.copyWith(
+                              border: Border.all(color: focusedBorderColor),
+                            ),
+                          ),
+                          submittedPinTheme: defaultPinTheme.copyWith(
+                            decoration: defaultPinTheme.decoration!.copyWith(
+                              color: fillColor,
+                              // borderRadius: BorderRadius.circular(19),
+                              border: Border.all(color: focusedBorderColor),
+                            ),
+                          ),
+                          errorPinTheme: defaultPinTheme.copyBorderWith(
+                            border: Border.all(color: Colors.redAccent),
+                          ),
+                        ),
+                      ),
+                      RectangleButton(
+                        title: Constants.continueText,
+                        width: SizeConfig.screenWidth * 0.8,
+                        onPressed: () => Navigator.pushNamed(context, '/otp'),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: SizeConfig.blockSizeVertical,
+                ),
+
+
+              ],
+            ),
+
+            // TODO: Implement waves and remove them if keyboard is shown
+            const Flexible(
+              child: Positioned(
+                bottom: 0,
+                child: Text('Incomplete'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
